78e674dd3b61f96c9a39504160adac392a37f939
Fidel Leal <127764786+fidel-techprep@users.noreply.github.com>
2023-10-20 04:13:56 +0000

    add timezone to user model and registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :contacts
   resources :students
   resources :calendars
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations'}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
