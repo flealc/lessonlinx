bc2adadbaf90c31454bc59b50b9247f9918dfe34
Fidel Leal <fidelxleal@gmail.com>
2023-10-28 21:52:01 -0500

    lesson details working

--- app/javascript/controllers/resize_list_controller.js
+++ app/javascript/controllers/resize_list_controller.js
@@ -2,8 +2,30 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="resize-list"
 export default class extends Controller {
+
+  static targets = ["lessonsList", "lessonDetails", "listToggle"];
+
   connect() {
-    console.log("Hello controller")
+    document.documentElement.addEventListener("turbo:frame-load", () => {
+      this.toggle()
+    });
+  }
+  toggle() {
+   
+    
+    this.lessonsListTarget.hidden = !this.lessonsListTarget.hidden
+
+    if (this.lessonsListTarget.hidden) {
+      this.lessonDetailsTarget.classList.replace("col-9", "col-12")
+      this.lessonDetailsTarget.classList.replace("col-sm-7", "col-sm-10")
+      this.listToggleTarget.innerText = "Show lessons"
+    } else {
+      this.lessonDetailsTarget.classList.replace("col-12", "col-9")
+      this.lessonDetailsTarget.classList.replace("col-sm-10", "col-sm-7")
+      this.listToggleTarget.innerText = "Hide lessons"
+    }
+    
+    
   }
 
 }
