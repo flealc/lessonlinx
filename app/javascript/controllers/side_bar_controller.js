2c739a85ea8e9bf2c6e80e99c8cf8b368d3efee4
Fidel Leal <fidelxleal@gmail.com>
2023-12-01 18:40:00 -0600

    tweak stylesheet, buttons, navbar, sidebar

--- app/javascript/controllers/side_bar_controller.js
+++ app/javascript/controllers/side_bar_controller.js
@@ -10,33 +10,29 @@ export default class extends Controller {
   connect() {}
 
   toggle() {
-    this.collapsibleListTarget.hidden = !this.collapsibleListTarget.hidden
-
-    if (this.collapsibleListTarget.hidden) {
-      this.contentAreaTarget.classList.replace("col-9", "col-12")
-      this.contentAreaTarget.classList.replace("col-sm-7", "col-sm-9")
-      this.toggleIconTarget.classList.replace("fa-angle-left", "fa-angle-right")
-    } else {
-      this.contentAreaTarget.classList.replace("col-12", "col-9")
-      this.contentAreaTarget.classList.replace("col-sm-9", "col-sm-7")
-      this.toggleIconTarget.classList.replace("fa-angle-right", "fa-angle-left")
-    }
+    const isHidden = this.collapsibleListTarget.hidden;
+    this.collapsibleListTarget.hidden = !isHidden;
+    this.toggleContentArea(isHidden);
+    this.toggleIcons(isHidden ? "fa-angle-right" : "fa-angle-left", isHidden ? "fa-angle-left" : "fa-angle-right")
   }
 
   toggleFullPage() {
-   
-    this.collapsibleListTarget.hidden = !this.collapsibleListTarget.hidden
+    const isHidden = this.collapsibleListTarget.hidden
+    this.collapsibleListTarget.hidden = !isHidden
+    this.toggleContentArea(isHidden, true)
+    this.toggleIcons(isHidden ? "fa-angle-right" : "fa-angle-left", isHidden ? "fa-angle-left" : "fa-angle-right")
+  }
 
-    if (this.collapsibleListTarget.hidden) {
-      this.contentAreaTarget.classList.replace("col-9", "col-12")
-      this.contentAreaTarget.classList.replace("col-sm-10", "col-sm-12")
-      this.toggleIconTarget.classList.replace("fa-angle-left", "fa-angle-right")
-    } else {
-      this.contentAreaTarget.classList.replace("col-12", "col-9")
-      this.contentAreaTarget.classList.replace("col-sm-12", "col-sm-10")
-      this.toggleIconTarget.classList.replace("fa-angle-right", "fa-angle-left")
-    }
-    
+  toggleContentArea(isListHidden, fullPage = false) {
+    const colSizes = fullPage ? ["col-9", "col-12", "col-sm-10", "col-sm-12"] : ["col-9", "col-12", "col-sm-7", "col-sm-9"]
+    this.contentAreaTarget.classList.replace(isListHidden ? colSizes[1] : colSizes[0], isListHidden ? colSizes[0] : colSizes[1])
+    this.contentAreaTarget.classList.replace(isListHidden ? colSizes[3] : colSizes[2], isListHidden ? colSizes[2] : colSizes[3])
+  }
+
+  toggleIcons(oldIcon, newIcon) {
+    this.toggleIconTargets.forEach(icon => {
+      icon.classList.replace(oldIcon, newIcon)
+    })
   }
 
 }
