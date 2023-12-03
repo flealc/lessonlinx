d5999bfce4620d4c43fcadfdcb0f2277a2054d21
Fidel Leal <fidelxleal@gmail.com>
2023-12-03 12:06:33 -0600

    change sidebar collapse button placement, dashboard padding

--- app/javascript/controllers/side_bar_controller.js
+++ app/javascript/controllers/side_bar_controller.js
@@ -13,26 +13,24 @@ export default class extends Controller {
     const isHidden = this.collapsibleListTarget.hidden;
     this.collapsibleListTarget.hidden = !isHidden;
     this.toggleContentArea(isHidden);
-    this.toggleIcons(isHidden ? "fa-angle-right" : "fa-angle-left", isHidden ? "fa-angle-left" : "fa-angle-right")
+    this.toggleIcon(isHidden ? "fa-angles-right" : "fa-angles-left", isHidden ? "fa-angles-left" : "fa-angles-right")
   }
 
   toggleFullPage() {
     const isHidden = this.collapsibleListTarget.hidden
     this.collapsibleListTarget.hidden = !isHidden
     this.toggleContentArea(isHidden, true)
-    this.toggleIcons(isHidden ? "fa-angle-right" : "fa-angle-left", isHidden ? "fa-angle-left" : "fa-angle-right")
+    this.toggleIcon(isHidden ? "fa-angles-right" : "fa-angles-left", isHidden ? "fa-angles-left" : "fa-angles-right")
   }
 
   toggleContentArea(isListHidden, fullPage = false) {
-    const colSizes = fullPage ? ["col-9", "col-12", "col-sm-10", "col-sm-12"] : ["col-9", "col-12", "col-sm-7", "col-sm-9"]
+    const colSizes = fullPage ? ["col-9", "col-12", "col-sm-10", "col-sm-12"] : ["col-9", "col-12", "col-sm-8", "col-sm-10"]
     this.contentAreaTarget.classList.replace(isListHidden ? colSizes[1] : colSizes[0], isListHidden ? colSizes[0] : colSizes[1])
     this.contentAreaTarget.classList.replace(isListHidden ? colSizes[3] : colSizes[2], isListHidden ? colSizes[2] : colSizes[3])
   }
 
-  toggleIcons(oldIcon, newIcon) {
-    this.toggleIconTargets.forEach(icon => {
-      icon.classList.replace(oldIcon, newIcon)
-    })
+  toggleIcon(oldIcon, newIcon) {
+    this.toggleIconTarget.classList.replace(oldIcon, newIcon)
   }
 
 }
