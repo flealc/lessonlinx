5567c70df48377bc2ffe5a61483b1938f90ee561
Fidel Leal <fidelxleal@gmail.com>
2023-12-06 22:46:40 -0600

    change sidebar icon

--- app/javascript/controllers/side_bar_controller.js
+++ app/javascript/controllers/side_bar_controller.js
@@ -7,30 +7,40 @@ export default class extends Controller {
     "toggleIcon",
     "contentArea"
   ]
-  connect() {}
 
+  static values = {
+    show: String,
+    hide: String,
+  }
+
+  connect() {
+  }
+
+  
   toggle() {
     const isHidden = this.collapsibleListTarget.hidden;
     this.collapsibleListTarget.hidden = !isHidden;
     this.toggleContentArea(isHidden);
-    this.toggleIcon(isHidden ? "fa-angles-right" : "fa-angles-left", isHidden ? "fa-angles-left" : "fa-angles-right")
+    this.toggleIcon(isHidden ? this.hideValue : this.showValue)
   }
-
+  
   toggleFullPage() {
     const isHidden = this.collapsibleListTarget.hidden
     this.collapsibleListTarget.hidden = !isHidden
     this.toggleContentArea(isHidden, true)
-    this.toggleIcon(isHidden ? "fa-angles-right" : "fa-angles-left", isHidden ? "fa-angles-left" : "fa-angles-right")
+    this.toggleIcon(isHidden ? this.hideValue : this.showValue)
   }
-
+  
+  
+  
   toggleContentArea(isListHidden, fullPage = false) {
     const colSizes = fullPage ? ["col-9", "col-12", "col-sm-10", "col-sm-12"] : ["col-9", "col-12", "col-sm-8", "col-sm-10"]
     this.contentAreaTarget.classList.replace(isListHidden ? colSizes[1] : colSizes[0], isListHidden ? colSizes[0] : colSizes[1])
     this.contentAreaTarget.classList.replace(isListHidden ? colSizes[3] : colSizes[2], isListHidden ? colSizes[2] : colSizes[3])
   }
-
-  toggleIcon(oldIcon, newIcon) {
-    this.toggleIconTarget.classList.replace(oldIcon, newIcon)
+  
+  toggleIcon(newIcon) {
+    this.toggleIconTarget.setAttribute("src", newIcon)
   }
 
 }
